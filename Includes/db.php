6cd0f22b2b38adf74ece9d87e00d3d0157a3e2e1
Abhishek Gupta <abhig0209@gmail.com>
2020-06-23 01:25:30 +0530

    Project ready for deployment

--- Includes/db.php
+++ Includes/db.php
@@ -1,7 +1,7 @@
 <?php 
 
-        // $con = mysqli_connect("localhost","root","","impulse101");
-        $con = mysqli_connect("sql101.epizy.com","epiz_26079797","Cr21no5EEOK","epiz_26079797_agrocraft");
+        $con = mysqli_connect("localhost","root","","impulse101");
+      
         if (mysqli_connect_errno()) {
                 echo "Failed to connect to MySql " . mysqli_connect_error();
         }
